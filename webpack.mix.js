e88504381d860dec93b86888b6ca1ec9c29bdc32
Mark Woodward <markw@digitalbridge.com.au>
2020-09-20 11:15:57 +1000

    restructure assets, laravel-mix, webpack

---
+++ webpack.mix.js
@@ -0,0 +1,34 @@
+const mix = require('laravel-mix');
+const ora = require('ora')
+require('laravel-mix-merge-manifest');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+var spinner = ora('Building assets...')
+spinner.start()
+
+
+// Assets build
+mix.js('resources/js/app.js', 'public/js')
+  .extract(['jquery', 'lodash', 'svg.js', 'jquery-validation', 'slick-carousel', 'sweetalert2'], 'src/js/vendor.js')
+  .sass('resources/sass/vendor.scss', 'src/css')
+  .sass('resources/sass/app.scss', 'src/css')
+  .sourceMaps()
+  .mergeManifest()
+  .then(function () {
+    spinner.stop()
+  });
+
+
+if (mix.inProduction()) {
+  mix.version();
+}
